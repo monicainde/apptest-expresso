067f056762f662efa29bea3f0008896e90f2083c
Monica <monique.inde@gmail.com>
2018-09-04 15:40:21 +0200

    Seperated tests from example file
    Consolidated A workflow in single test

--- AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -2,45 +2,10 @@ package com.mytaxi.android_demo;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.contrib.DrawerActions;
-import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
-
-import android.app.Activity;
-import com.mytaxi.android_demo.activities.MainActivity;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.UiObject;
-import android.support.test.uiautomator.UiObjectNotFoundException;
-import android.support.test.uiautomator.UiSelector;
-
-
-import com.mytaxi.android_demo.activities.MainActivity;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.app.PendingIntent.getActivity;
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 /**
@@ -52,23 +17,6 @@ import static org.junit.Assert.*;
 @LargeTest
 public class ExampleInstrumentedTest {
 
-    UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-            MainActivity.class);
-
-    @Before
-    public void beforeEachTest(){
-        System.out.println("Test Started");
-    }
-
-
-    @AfterClass
-    public static void afterEachTest(){
-        System.out.println("Class Ended");
-        logOut();
-    }
-
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -78,35 +26,5 @@ public class ExampleInstrumentedTest {
     }
 
 
-
-    public static void logOut() {
-        //Open the drawer and hit logout
-        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
-        onView(withText("Logout")).perform(click());
-    }
-
-    @Test
-    public void searchDriver() {
-        // Type text and then press the search button.
-        onView(withId(R.id.textSearch))
-                .perform(typeText("sa"), closeSoftKeyboard());
-
-        //onData(equalTo("Sarah Scott")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        onView(withText("Sarah Scott"))
-                .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
-                .perform(click());
-        //Click on Call Button
-        onView(withId(R.id.fab))
-                .perform(click());
-        //Test Assertion - Validate that the driver's number is dialed on the screen
-        UiObject dialedNumberTextelement = mDevice.findObject(new UiSelector().text("(413) 868-2228"));
-        assertThat(dialedNumberTextelement.exists(), equalTo(true));
-
-        //come back to the app as user might do - by pressing back button
-        mDevice.pressBack();
-        mDevice.pressBack();
-        mDevice.pressBack();
-        mDevice.pressBack();
-    }
 
 }
--- AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/LogInTests.java
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/LogInTests.java
@@ -1,5 +1,6 @@
 package com.mytaxi.android_demo;
 
+import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -21,8 +22,14 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -51,7 +58,7 @@ public class LogInTests {
         }
     }
 
-    @Test
+    //@Test
     public void logIn() {
         // Type username/password and then press the log in button.
         onView(withId(R.id.edt_username))
@@ -66,6 +73,44 @@ public class LogInTests {
         }
         // Check that landing page is loaded
         onView(withId(R.id.textSearch)).check(matches(isDisplayed()));
+    }
+
+    //@Test
+    public void searchDriver() {
+        // Type text and then press the search button.
+        onView(withId(R.id.textSearch))
+                .perform(typeText("sa"), closeSoftKeyboard());
+
+        //onData(equalTo("Sarah Scott")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
+        onView(withText("Sarah Scott"))
+                .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
+                .perform(click());
+        //Click on Call Button
+        onView(withId(R.id.fab))
+                .perform(click());
+        //Test Assertion - Validate that the driver's number is dialed on the screen
+        UiObject dialedNumberTextelement = mDevice.findObject(new UiSelector().text("(413) 868-2228"));
+        assertThat(dialedNumberTextelement.exists(), equalTo(true));
+
+        //come back to the app as user might do - by pressing back button
+        mDevice.pressBack();
+        mDevice.pressBack();
+        mDevice.pressBack();
+        mDevice.pressBack();
+    }
+
+    //@Test
+    public void logOut() {
+        //Open the drawer and hit logout
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText("Logout")).perform(click());
+    }
+
+    @Test
+    public void logInAndSearchDriver() {
+        logIn();
+        searchDriver();
+        logOut();
     }
 
 }
