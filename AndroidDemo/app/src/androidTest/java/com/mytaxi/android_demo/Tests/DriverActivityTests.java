1cda12d3ae26f5fb9355693630d376caa7fd0da3
Monica <monique.inde@gmail.com>
2018-09-05 13:25:45 +0200

    Seperated WaitHelper from Tests

---
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/Tests/DriverActivityTests.java
@@ -0,0 +1,113 @@
+package com.mytaxi.android_demo.Tests;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
+
+import com.mytaxi.android_demo.R;
+import com.mytaxi.android_demo.Tests.LogInTests;
+
+import com.mytaxi.android_demo.activities.MainActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+@LargeTest
+public class DriverActivityTests {
+    private String DRIVER_NAME;
+    private String DRIVER_NUMBER;
+    private UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
+    public LogInTests logIn_Tests;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "Sarah Scott", "(413) 868-2228" }
+        });
+    }
+
+    //Constructor
+    public DriverActivityTests(String driverName, String driverNumber){
+        this.DRIVER_NAME = driverName;
+        this.DRIVER_NUMBER = driverNumber;
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initialSetUp(){
+        //set up log in parameters
+        logIn_Tests = new LogInTests("crazydog335","venture");
+    }
+
+    @After
+    public void tearDown(){
+        logIn_Tests.logOut();
+    }
+
+    public void searchDriver() {
+        // Type text and then press the search button.
+        String driverSearchString= DRIVER_NAME.substring(0,2).toLowerCase();
+        onView(ViewMatchers.withId(R.id.textSearch))
+                .perform(typeText(driverSearchString), closeSoftKeyboard());
+
+        //select the 2nd result (via the name) from the list
+        onView(withText(DRIVER_NAME))
+                .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
+                .perform(click());
+        //Click on Call Button
+        onView(withId(R.id.fab))
+                .perform(click());
+        //Test Assertion - Validate that the driver's number is dialed on the screen
+        UiObject dialedNumberTextelement = mDevice.findObject(new UiSelector().text(DRIVER_NUMBER));
+        assertThat(dialedNumberTextelement.exists(), equalTo(true));
+
+        //come back to the main app, from contacts app
+        goBackToAppHome();
+    }
+
+    public void goBackToAppHome() {
+        //come back to the app as user might do - by pressing back button
+        UiObject topNavigationBar = mDevice.findObject(new UiSelector().text("mytaxi demo"));
+        int backpressCounter = 0; //to avoid infinite loops
+        while(!topNavigationBar.exists()) {
+            mDevice.pressBack();
+            backpressCounter++;
+            if(backpressCounter>10)
+                break;
+        }
+    }
+
+    @Test
+    public void logInAndSearchDriver() {
+        logIn_Tests.logIn();
+        searchDriver();
+    }
+
+}
--- AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/Tests/LogInTests.java
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/Tests/LogInTests.java
@@ -1,6 +1,7 @@
-package com.mytaxi.android_demo;
+package com.mytaxi.android_demo.Tests;
 
 import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,12 +10,18 @@ import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
 
+import com.mytaxi.android_demo.R;
 import com.mytaxi.android_demo.activities.MainActivity;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
@@ -22,11 +29,10 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
@@ -34,9 +40,20 @@ import static org.junit.Assert.assertThat;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class LogInTests {
-    public String USERNAME = "crazydog335";
-    public String PASSWORD = "venture";
-    UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
+    private String USERNAME;
+    private String PASSWORD;
+    private UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
+
+    public LogInTests(String username, String password){
+        this.USERNAME = username;
+        this.PASSWORD = password;
+    }
+
+    /*
+    public LogInTests(){
+        this.USERNAME = "crazydog335";
+        this.PASSWORD = "venture";
+    }*/
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
@@ -44,7 +61,7 @@ public class LogInTests {
 
     public void allowCurrentPermission() throws UiObjectNotFoundException {
         UiObject allowButton = mDevice.findObject(new UiSelector().text("ALLOW"));
-        //new UiSelector().
+        //if its a first time execution of the test, then only you get this popup - Allowand go ahead
         if(allowButton.exists())
             allowButton.click();
     }
@@ -58,59 +75,27 @@ public class LogInTests {
         }
     }
 
-    //@Test
+    @Test
     public void logIn() {
         // Type username/password and then press the log in button.
-        onView(withId(R.id.edt_username))
+        onView(ViewMatchers.withId(R.id.edt_username))
                 .perform(typeText(USERNAME), closeSoftKeyboard());
         onView(withId(R.id.edt_password))
                 .perform(typeText(PASSWORD), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        //Sometime the login takes too long and landing page does not load immediately, waitForId will wait till textSearch is loaded.
+        onView(isRoot()).perform(WaitHelper.waitForId(R.id.textSearch, TimeUnit.SECONDS.toMillis(10)));
+
         // Check that landing page is loaded
         onView(withId(R.id.textSearch)).check(matches(isDisplayed()));
     }
 
-    //@Test
-    public void searchDriver() {
-        // Type text and then press the search button.
-        onView(withId(R.id.textSearch))
-                .perform(typeText("sa"), closeSoftKeyboard());
-
-        //onData(equalTo("Sarah Scott")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        onView(withText("Sarah Scott"))
-                .inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
-                .perform(click());
-        //Click on Call Button
-        onView(withId(R.id.fab))
-                .perform(click());
-        //Test Assertion - Validate that the driver's number is dialed on the screen
-        UiObject dialedNumberTextelement = mDevice.findObject(new UiSelector().text("(413) 868-2228"));
-        assertThat(dialedNumberTextelement.exists(), equalTo(true));
-
-        //come back to the app as user might do - by pressing back button
-        mDevice.pressBack();
-        mDevice.pressBack();
-        mDevice.pressBack();
-        mDevice.pressBack();
-    }
-
-    //@Test
+    @Test
     public void logOut() {
         //Open the drawer and hit logout
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withText("Logout")).perform(click());
-    }
-
-    @Test
-    public void logInAndSearchDriver() {
-        logIn();
-        searchDriver();
-        logOut();
     }
 
 }
